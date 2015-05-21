1189943a9d7ce447acb8d631bf28fe01fc5e9f33
Mathias Buus <mathiasbuus@gmail.com>
2015-05-21 14:26:18 +0200

    mount special file systems

--- index.js
+++ index.js
@@ -12,18 +12,28 @@ boot.command = function (cmd, opts) {
   var dir = opts.cwd || process.cwd()
   var volumes = opts.volumes || {}
   var cmds = []
-  var procPath = JSON.stringify(path.join(dir, 'proc'))
+  var cleanup = []
 
-  cmds.push('mount -t proc ' + procPath + ' ' + procPath)
+  var procPath = JSON.stringify(path.resolve(path.join(dir, 'proc')))
+  var sysPath = JSON.stringify(path.resolve(path.join(dir, 'sys')))
+  var devPath = JSON.stringify(path.resolve(path.join(dir, 'dev')))
+
+  cmds.push('mount -t proc proc ' + procPath)
+  cmds.push('mount -t sysfs sys ' + sysPath)
+  cmds.push('mount -t bind /dev ' + devPath)
+
+  cleanup.push('umount ' + procPath)
+  cleanup.push('umount ' + sysPath)
+  cleanup.push('umount ' + devPath)
 
   Object.keys(volumes).forEach(function (host) {
     var childPath = JSON.stringify(path.join(dir, volumes[host]))
-    var hostPath = JSON.stringify(host)
+    var hostPath = JSON.stringify(path.resolve(host))
     cmds.push('mkdir -p ' + childPath)
     cmds.push('mount --bind ' + hostPath + ' ' + childPath)
   })
 
-  return '/bin/sh -c \'' + cmds.join('; ') + '\' >/dev/null 2>&1; chroot . ' + cmd
+  return '/bin/sh -c \'' + cmds.join('; ') + '\' >/dev/null 2>&1; chroot . ' + cmd + '; ' + cleanup.join('; ')
 }
 
 module.exports = boot
